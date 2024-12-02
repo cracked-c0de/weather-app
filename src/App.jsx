766e5bd9c2bcd5bea59cc4a15d248e300d9f03e0
Oybek <itpustudent@gmail.com>
2024-12-02 18:50:43 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Weather from "./components/Weather"
 function App() {
   return(
     <div className='app'>
-      <Weather/>
+      {window.innerWidth >= 500 && window.innerHeight >= 600 ? <Weather/> : <h1 className="pc-only">Please, Use Desktop/Laptop to Enter this web page!</h1>}
+      
     </div>
   )
 }
